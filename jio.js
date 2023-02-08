f7b37fe3b83eebcf76719810e92d12caf38e0092
nishaj8694 <nishajppvvm@gmail.com>
2023-02-08 13:54:29 +0530

    commit

---
+++ jio.js
@@ -0,0 +1,18 @@
+
+function change(){
+    alert("our custmer will be contacting you");
+}
+
+let a=document.getElementById()
+
+(function ($) {
+    $('[data-toggle="counter-up"]').counterUp({
+        delay: 10,
+        time: 2000
+    });
+})(jQuery);
+
+function colr(){
+    var a=document.getElementById("dem");
+    a.style.color="blue";
+}
